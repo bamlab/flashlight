e9471929bfa06e34edfbc33687000f0c92994d3d
almouro <contact@almouro.com>
2022-06-30 15:39:35 +0200

    chore: change logging

--- packages/android-performance-profiler/src/commands/cpu/getCpuClockTick.ts
+++ packages/android-performance-profiler/src/commands/cpu/getCpuClockTick.ts
@@ -1,10 +1,11 @@
+import { Logger } from "@performance-profiler/logger";
 import { executeCommand } from "../shell";
 
 export const getCpuClockTick = () => {
   try {
     return parseInt(executeCommand(`adb shell getconf CLK_TCK`), 10);
   } catch (error) {
-    console.error(error);
+    Logger.warn("Failed to retrieve CPU clock tick, defaulting to 100");
     return 100;
   }
 };
