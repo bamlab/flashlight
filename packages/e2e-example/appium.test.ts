2e298ce2f9295355507f01fd5d468b80d78abe29
almouro <contact@almouro.com>
2022-07-03 17:09:39 +0200

    test: rewrite sample appium test

--- packages/e2e-example/appium.test.ts
+++ packages/e2e-example/appium.test.ts
@@ -2,38 +2,67 @@
 
 import { AppiumDriver } from "@bam.tech/appium-helper";
 import { TestCase, PerformanceTester } from "@bam.tech/e2e-performance";
-import {
-  getAverageCpuUsage,
-  getHighCpuUsageStats,
-} from "android-performance-profiler";
 
-test("e2e", async () => {
-  const bundleId = "com.example";
+const BundleId = {
+  FLUTTER: "com.example.clash_of_techs_twitter_feed",
+  RN: "com.reactnativefeed",
+  NATIVE: "tech.bam.twitbench",
+};
+
+const bundleId = BundleId.NATIVE;
+
+const getTestCases = async () => {
   const driver = await AppiumDriver.create({
     appPackage: bundleId,
     appActivity: `${bundleId}.MainActivity`,
   });
 
   const startAppTestCase: TestCase = {
-    beforeTest: () => driver.stopApp(),
+    beforeTest: async () => {
+      driver.stopApp();
+      await driver.wait(3000);
+    },
     run: async () => {
       driver.startApp();
-      await driver.findElementByText("My Text");
+
+      if (bundleId === BundleId.FLUTTER) {
+        // await driver.client.$(`//android.widget.ImageView[@content-desc=""]`);
+      } else {
+        await driver.findElementByText("Notre CEO");
+      }
     },
   };
 
+  const scrollTestCase: TestCase = {
+    beforeTest: async () => {
+      // Restart app
+      await startAppTestCase.beforeTest?.();
+      await startAppTestCase.run();
+    },
+    run: async () => {
+      for (let index = 0; index < 10; index++) {
+        await driver.gestures.swipeUp();
+        await driver.wait(1000);
+      }
+    },
+  };
+
+  return {
+    START: startAppTestCase,
+    SCROLL: scrollTestCase,
+  };
+};
+
+test.skip("e2e", async () => {
+  const testCases = await getTestCases();
+
   const measures = await new PerformanceTester(bundleId).iterate(
-    startAppTestCase
+    testCases.SCROLL,
+    10
   );
 
-  for (const measure of measures) {
-    const message = `=====MEASURE====
-    ${measure.coldStartTime}ms
-    ${JSON.stringify(getHighCpuUsageStats(measure.cpuMeasures), null, 2)}
-    ${
-      getAverageCpuUsage(measure.cpuMeasures) * measure.cpuMeasures.length * 0.5
-    }
-    `;
-    console.log(message);
-  }
+  require("fs").writeFileSync(
+    `./results_${new Date().getTime()}.json`,
+    JSON.stringify(measures)
+  );
 });
