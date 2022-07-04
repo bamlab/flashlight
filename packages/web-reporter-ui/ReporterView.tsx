90a23050a00500a70dca2aecb978c5b2978717fa
almouro <contact@almouro.com>
2022-07-04 17:23:09 +0200

    ui: reorganize sections

--- packages/web-reporter-ui/ReporterView.tsx
+++ packages/web-reporter-ui/ReporterView.tsx
@@ -3,13 +3,26 @@ import { Measure } from "android-performance-profiler";
 import { CPUReport } from "./CPUReport";
 import { ReportSummary } from "./ReportSummary";
 import { RAMReport } from "./RAMReport";
+import { AccordionSectionTitle } from "./components/AccordionSectionTitle";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
 
 const Report = ({ measures }: { measures: Measure[] }) => {
   return (
     <>
       <ReportSummary measures={measures} />
-      <CPUReport measures={measures} />
-      <RAMReport measures={measures} />
+      <Accordion>
+        <AccordionSectionTitle title="CPU" />
+        <AccordionDetails>
+          <CPUReport measures={measures} />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSectionTitle title="RAM" />
+        <AccordionDetails>
+          <RAMReport measures={measures} />
+        </AccordionDetails>
+      </Accordion>
     </>
   );
 };
