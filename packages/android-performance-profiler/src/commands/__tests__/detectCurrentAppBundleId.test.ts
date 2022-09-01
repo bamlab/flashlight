e76313ebe625991008bc366335775baf4b96a977
MaeIg <larochmael@hotmail.fr>
2022-09-01 14:37:35 +0200

    fix(profiler): detect appActivity when command output doesn't contain "$"
    On my phone, this command fails because output is Surface(name=com.example.staging/com.example.MainActivity)
    and not Surface(name=com.example.staging/com.example.MainActivity$_0876)

--- packages/android-performance-profiler/src/commands/__tests__/detectCurrentAppBundleId.test.ts
+++ packages/android-performance-profiler/src/commands/__tests__/detectCurrentAppBundleId.test.ts
@@ -4,16 +4,35 @@ const sampleOutput = `
 mSurface=Surface(name=com.example.staging/com.example.MainActivity$_9617)/@0x993d3ae
 mSurface=Surface(name=com.sec.android.app.launcher/com.sec.android.app.launcher.activities.LauncherActivity$_3088)/@0x469a915`;
 
+const sampleOutputWithoutDollars = `
+mSurface=Surface(name=com.example.staging/com.example.MainActivity)/@0x993d3ae
+mSurface=Surface(name=com.sec.android.app.launcher/com.sec.android.app.launcher.activities.LauncherActivity)/@0x469a915`;
+
 const executeCommandSpy = jest.spyOn(require("../shell"), "executeCommand");
 
 describe("detectCurrentAppBundleId", () => {
-  it("retrieves correctly bundle id and app activity", () => {
+  it("retrieves correctly bundle id and app activity when result match 'name=appId/appActivity$'", () => {
     executeCommandSpy.mockImplementation((command) => {
       expect(command).toEqual(
         "adb shell dumpsys window windows | grep -E 'mCurrentFocus|mFocusedApp|mInputMethodTarget|mSurface' | grep Activity"
       );
 
       return sampleOutput;
+    });
+
+    expect(detectCurrentAppBundleId()).toEqual({
+      bundleId: "com.example.staging",
+      appActivity: "com.example.MainActivity",
+    });
+  });
+
+  it("retrieves correctly bundle id and app activity when result match 'name=appId/appActivity)'", () => {
+    executeCommandSpy.mockImplementation((command) => {
+      expect(command).toEqual(
+        "adb shell dumpsys window windows | grep -E 'mCurrentFocus|mFocusedApp|mInputMethodTarget|mSurface' | grep Activity"
+      );
+
+      return sampleOutputWithoutDollars;
     });
 
     expect(detectCurrentAppBundleId()).toEqual({
