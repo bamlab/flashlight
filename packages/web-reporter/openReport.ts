78be892c6d7b377f446a4344c65c6eb675733375
almouro <contact@almouro.com>
2022-09-14 11:24:20 +0200

    ref: extract writeReport function

--- packages/web-reporter/openReport.ts
+++ packages/web-reporter/openReport.ts
@@ -18,49 +18,62 @@ program
 
 program.parse();
 
-const newJsFile = "report.js";
+const writeReport = ({
+  jsonPaths,
+  outputDir,
+}: {
+  jsonPaths: string[];
+  outputDir: string;
+}) => {
+  const newJsFile = "report.js";
 
-const oldHtmlContent = fs.readFileSync(`${__dirname}/index.html`, "utf8");
-const scriptName = oldHtmlContent.match(/src="(.*?)"/)?.[1];
+  const oldHtmlContent = fs.readFileSync(`${__dirname}/index.html`, "utf8");
+  const scriptName = oldHtmlContent.match(/src="(.*?)"/)?.[1];
 
-const newHtmlContent = fs
-  .readFileSync(`${__dirname}/index.html`, "utf8")
-  .replace(`src="${scriptName}"`, `src="${newJsFile}"`)
-  .replace('type="module"', "");
+  const newHtmlContent = fs
+    .readFileSync(`${__dirname}/index.html`, "utf8")
+    .replace(`src="${scriptName}"`, `src="${newJsFile}"`)
+    .replace('type="module"', "");
 
-const getJsonPaths = () => {
-  const paths = program.args;
+  const getJsonPaths = () => {
+    return jsonPaths
+      .map((path) => {
+        const isDirectory = fs.lstatSync(path).isDirectory();
 
-  return paths
-    .map((path) => {
-      const isDirectory = fs.lstatSync(path).isDirectory();
+        if (isDirectory) {
+          return fs
+            .readdirSync(path)
+            .filter((file) => file.endsWith(".json"))
+            .map((file) => `${path}/${file}`);
+        } else {
+          return path;
+        }
+      })
+      .flat();
+  };
 
-      if (isDirectory) {
-        return fs
-          .readdirSync(path)
-          .filter((file) => file.endsWith(".json"))
-          .map((file) => `${path}/${file}`);
-      } else {
-        return path;
-      }
-    })
-    .flat();
+  const report = JSON.stringify(
+    getJsonPaths().map((path) => JSON.parse(fs.readFileSync(path, "utf8")))
+  );
+
+  const jsFileContent = fs
+    .readFileSync(`${__dirname}/${scriptName}`, "utf8")
+    .replace('"INSERT_HERE"', report);
+
+  fs.writeFileSync(`${outputDir}/report.js`, jsFileContent);
+
+  const htmlFilePath = `${outputDir}/report.html`;
+  fs.writeFileSync(htmlFilePath, newHtmlContent);
+
+  return htmlFilePath;
 };
 
-const report = JSON.stringify(
-  getJsonPaths().map((path) => JSON.parse(fs.readFileSync(path, "utf8")))
-);
-
-const jsFileContent = fs
-  .readFileSync(`${__dirname}/${scriptName}`, "utf8")
-  .replace('"INSERT_HERE"', report);
-
 const outputDir = (program.opts().outputDir as string) || __dirname;
-
-fs.writeFileSync(`${outputDir}/report.js`, jsFileContent);
-
-const htmlFilePath = `${outputDir}/report.html`;
-fs.writeFileSync(htmlFilePath, newHtmlContent);
+const jsonPaths = program.args;
+const htmlFilePath = writeReport({
+  outputDir,
+  jsonPaths,
+});
 
 Logger.success(`Opening report: ${htmlFilePath}`);
 execSync(`open ${htmlFilePath}`);
