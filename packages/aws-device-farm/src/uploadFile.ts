ba8618260c0662bc6581123b123ac6418c020ccf
Alexandre Moureaux <Almouro@users.noreply.github.com>
2022-09-14 10:38:52 +0200

    feat(aws): add progress indicator for uploads (#25)

--- packages/aws-device-farm/src/uploadFile.ts
+++ packages/aws-device-farm/src/uploadFile.ts
@@ -1,24 +1,11 @@
-import fs from "fs";
-import axios from "axios";
 import { Logger } from "@perf-profiler/logger";
+import { execSync } from "child_process";
 
 export const uploadFile = async (url: string, filePath: string) => {
   Logger.info(`Uploading ${filePath}...`);
-
-  const read_stream = fs.createReadStream(filePath);
-  const { size } = fs.statSync(filePath);
-
-  await axios({
-    method: "put",
-    url: url,
-    maxContentLength: Infinity,
-    maxBodyLength: Infinity,
-    headers: {
-      "Content-Length": size,
-    },
-    data: read_stream,
-    onUploadProgress: (progressEvent) => console.log(progressEvent),
-  });
-
+  execSync(
+    `curl -T ${filePath} "${url}" --fail --progress-bar -o /tmp/upload_${new Date().getTime()}.txt`,
+    { stdio: "inherit" }
+  );
   Logger.info(`Upload of ${filePath} done`);
 };
