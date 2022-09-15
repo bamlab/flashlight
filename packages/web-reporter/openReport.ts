4b83771c916da8b433222a6376b6b1180edfc42d
Alexandre Moureaux <Almouro@users.noreply.github.com>
2022-09-15 15:05:04 +0200

    fix(web-reporter): don't fail if open command doesn't exist (#26)
    * ref: extract writeReport function
    
    * ref: extract writeReport to separate file
    
    * chore: add eslintcache to gitignore
    
    * fix: don't fail if open doesn't exist

--- packages/web-reporter/openReport.ts
+++ packages/web-reporter/openReport.ts
@@ -3,7 +3,7 @@
 import { execSync } from "child_process";
 import { program } from "commander";
 import { Logger } from "@perf-profiler/logger";
-import fs from "fs";
+import { writeReport } from "./writeReport";
 
 program
   .argument("<files/folders...>")
@@ -18,49 +18,16 @@ program
 
 program.parse();
 
-const newJsFile = "report.js";
-
-const oldHtmlContent = fs.readFileSync(`${__dirname}/index.html`, "utf8");
-const scriptName = oldHtmlContent.match(/src="(.*?)"/)?.[1];
-
-const newHtmlContent = fs
-  .readFileSync(`${__dirname}/index.html`, "utf8")
-  .replace(`src="${scriptName}"`, `src="${newJsFile}"`)
-  .replace('type="module"', "");
-
-const getJsonPaths = () => {
-  const paths = program.args;
-
-  return paths
-    .map((path) => {
-      const isDirectory = fs.lstatSync(path).isDirectory();
-
-      if (isDirectory) {
-        return fs
-          .readdirSync(path)
-          .filter((file) => file.endsWith(".json"))
-          .map((file) => `${path}/${file}`);
-      } else {
-        return path;
-      }
-    })
-    .flat();
-};
-
-const report = JSON.stringify(
-  getJsonPaths().map((path) => JSON.parse(fs.readFileSync(path, "utf8")))
-);
-
-const jsFileContent = fs
-  .readFileSync(`${__dirname}/${scriptName}`, "utf8")
-  .replace('"INSERT_HERE"', report);
-
 const outputDir = (program.opts().outputDir as string) || __dirname;
-
-fs.writeFileSync(`${outputDir}/report.js`, jsFileContent);
-
-const htmlFilePath = `${outputDir}/report.html`;
-fs.writeFileSync(htmlFilePath, newHtmlContent);
+const jsonPaths = program.args;
+const htmlFilePath = writeReport({
+  outputDir,
+  jsonPaths,
+});
 
 Logger.success(`Opening report: ${htmlFilePath}`);
-execSync(`open ${htmlFilePath}`);
+try {
+  execSync(`open ${htmlFilePath}`);
+} catch {
+  Logger.warn(`Failed to run "open ${htmlFilePath}"`);
+}
