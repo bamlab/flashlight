16a10f6430dd0c8c37ef5b0dd55270e10401b883
almouro <contact@almouro.com>
2022-11-02 15:23:48 +0100

    feat(web-reporter): add ability to set time interval to 'cut' measures

--- packages/web-reporter/openReport.ts
+++ packages/web-reporter/openReport.ts
@@ -7,22 +7,36 @@ import { writeReport } from "./writeReport";
 
 program
   .argument("<files/folders...>")
-  .description("Generate web report from performance measures")
-  .usage(
-    `yarn generate-performance-web-report -o . results1.json results2.json`
+  .description(
+    `Generate web report from performance measures.
+
+Examples:
+npx @perf-profiler/web-reporter results1.json
+npx @perf-profiler/web-reporter results1.json results2.json -o output-dir
+npx @perf-profiler/web-reporter results1.json --skip 1500 --duration 10000
+`
   )
+  .option("-o, --output-dir <outputDir>", "Output directory for the web report")
   .option(
-    "-o, --output-dir <outputDir>",
-    "Output directory for the web report"
-  );
+    "-d, --duration <duration>",
+    'Duration in ms of measures to analyze in report. If measures are longer than that, they\'ll be "cut".'
+  )
+  .option("-s, --skip <skip>", "Skip first ms of measures in report");
 
 program.parse();
 
 const outputDir = (program.opts().outputDir as string) || __dirname;
+const duration = program.opts().duration
+  ? parseInt(program.opts().duration, 10)
+  : null;
+const skip = program.opts().skip ? parseInt(program.opts().skip, 10) : 0;
+
 const jsonPaths = program.args;
 const htmlFilePath = writeReport({
   outputDir,
   jsonPaths,
+  duration,
+  skip,
 });
 
 Logger.success(`Opening report: ${htmlFilePath}`);
--- packages/web-reporter/writeReport.ts
+++ packages/web-reporter/writeReport.ts
@@ -1,11 +1,55 @@
 import fs from "fs";
+import { TestCaseResult } from "@perf-profiler/types";
+
+// We should have better management of the time interval over the repo
+const MEASURE_INTERVAL = 500;
+
+const assertTimeIntervalMultiple = (n: number) => {
+  if (n % MEASURE_INTERVAL !== 0) {
+    throw new Error(
+      `Only multiples of the measure interval (${MEASURE_INTERVAL}ms) are supported`
+    );
+  }
+};
+
+export const getMeasuresForTimeInterval = ({
+  duration,
+  skip,
+  results,
+}: {
+  duration: number | null;
+  skip: number;
+  results: TestCaseResult[];
+}): TestCaseResult[] => {
+  assertTimeIntervalMultiple(skip);
+  if (duration !== null) assertTimeIntervalMultiple(duration);
+
+  const firstMeasureIndex = skip / MEASURE_INTERVAL;
+
+  return results.map((result) => ({
+    ...result,
+    iterations: result.iterations.map((iteration) => ({
+      ...iteration,
+      measures: iteration.measures.slice(
+        firstMeasureIndex,
+        duration
+          ? firstMeasureIndex + duration / MEASURE_INTERVAL + 1
+          : iteration.measures.length
+      ),
+    })),
+  }));
+};
 
 export const writeReport = ({
   jsonPaths,
   outputDir,
+  duration,
+  skip = 0,
 }: {
   jsonPaths: string[];
   outputDir: string;
+  duration: number | null;
+  skip: number;
 }) => {
   const newJsFile = "report.js";
 
@@ -34,8 +78,12 @@ export const writeReport = ({
       .flat();
   };
 
+  const results: TestCaseResult[] = getJsonPaths().map((path) =>
+    JSON.parse(fs.readFileSync(path, "utf8"))
+  );
+
   const report = JSON.stringify(
-    getJsonPaths().map((path) => JSON.parse(fs.readFileSync(path, "utf8")))
+    getMeasuresForTimeInterval({ results, skip, duration })
   );
 
   const jsFileContent = fs
