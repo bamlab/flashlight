47cd8e1a753d787f18356c19e56b4a38c83ef7b3
almouro <contact@almouro.com>
2023-02-03 11:15:04 +0100

    refactor(e2e): extract waitFor method

--- packages/e2e-performance/src/PerformanceMeasurer.ts
+++ packages/e2e-performance/src/PerformanceMeasurer.ts
@@ -5,27 +5,7 @@ import {
   pollPerformanceMeasures,
 } from "@perf-profiler/profiler";
 import { Trace } from "./Trace";
-
-const waitFor = async <T>(
-  evaluateResult: () => T | undefined | null,
-  { timeout, checkInterval }: { timeout: number; checkInterval: number } = {
-    timeout: 10000,
-    checkInterval: 50,
-  }
-): Promise<T> => {
-  if (timeout < 0) {
-    throw new Error("Waited for condition which never happened");
-  }
-  const result = evaluateResult();
-  if (result) return result;
-
-  await new Promise((resolve) => setTimeout(resolve, checkInterval));
-
-  return waitFor(evaluateResult, {
-    timeout: timeout - checkInterval,
-    checkInterval,
-  });
-};
+import { waitFor } from "./utils/waitFor";
 
 export class PerformanceMeasurer {
   measures: Measure[] = [];
