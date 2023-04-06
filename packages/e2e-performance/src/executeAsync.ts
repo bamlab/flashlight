442ff2015d51f2b95574703b18f4e4f4a350c2f7
almouro <contact@almouro.com>
2023-04-06 21:08:05 +0200

    fix(e2e): add windows support

--- packages/e2e-performance/src/executeAsync.ts
+++ packages/e2e-performance/src/executeAsync.ts
@@ -1,23 +1,30 @@
 import { spawn } from "child_process";
 import { Logger } from "@perf-profiler/logger";
 
-const isMacOs = process.platform === "darwin";
+// Running with `script` ensures we display to the terminal exactly what the original script does
+// For instance, maestro clears the screen multiple times which wouldn't work without calling `script`
+/**
+ * Running with `script` ensures we display to the terminal exactly what the original script does
+ * For instance, maestro clears the screen multiple times which wouldn't work without calling `script`
+ *
+ * on macOS you would run `script -q /dev/null echo "Running command"`,
+ * but on Linux, you would run `script -q /dev/null -c "echo \"Running command\""`
+ */
+const spawnProcess = (command: string) => {
+  const parts = command.split(" ");
+
+  switch (process.platform) {
+    case "darwin":
+      return spawn("script", ["-q", "/dev/null", ...parts]);
+    case "win32":
+      return spawn(parts[0], parts.slice(1));
+    default:
+      return spawn("script", ["-q", "/dev/null", ...["-e", "-c", command]]);
+  }
+};
 
 export const executeAsync = (command: string) => {
-  // Running with `script` ensures we display to the terminal exactly what the original script does
-  // For instance, maestro clears the screen multiple times which wouldn't work without calling `script`
-  /**
-   * Running with `script` ensures we display to the terminal exactly what the original script does
-   * For instance, maestro clears the screen multiple times which wouldn't work without calling `script`
-   *
-   * on macOS you would run `script -q /dev/null echo "Running command"`,
-   * but on Linux, you would run `script -q /dev/null -c "echo \"Running command\""`
-   */
-  const child = spawn("script", [
-    "-q",
-    "/dev/null",
-    ...(isMacOs ? command.split(" ") : ["-e", "-c", command]),
-  ]);
+  const child = spawnProcess(command);
 
   return new Promise((resolve, reject) => {
     child.stdout.on("data", (data: ReadableStream<string>) => {
