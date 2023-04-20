bb08d0e045db33c2000dbd35f7e4450138657837
Matthieu <62448416+mbezard@users.noreply.github.com>
2023-04-20 15:31:20 +0200

    feat(web): redesign reports/add collapsible metrics explanation (#94)
    * chore: add arrow icon from figma
    
    * chore: create an animationless Collapsible component
    
    * feat: use collapsible to display explanations for each metric
    
    * feat: add animation to collapsible
    
    * feat: better explanations for high cpu + put value and unit together in info row
    
    * refactor: move explanations into a separated file
    
    * feat: display impacted threads in bullet points
    
    * fix: use correct svg prop
    
    * fix: better animation + make collapsible stylable

---
+++ packages/web-reporter-ui/src/components/Collapsible.tsx
@@ -0,0 +1,57 @@
+import React, {
+  FunctionComponent,
+  PropsWithChildren,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { ArrowDownIcon } from "./icons/ArrowDownIcon";
+
+type Props = PropsWithChildren<{
+  header: React.ReactNode;
+  className?: string;
+}>;
+
+export const Collapsible: FunctionComponent<Props> = ({
+  header,
+  className,
+  children,
+}) => {
+  const childrenContainerRef = useRef<HTMLDivElement>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleIsExpanded = useCallback(
+    () => setIsExpanded((prevIsExpanded) => !prevIsExpanded),
+    []
+  );
+  const childrenContainerStyle = useMemo(
+    () => ({
+      height: isExpanded ? childrenContainerRef.current?.scrollHeight : 0,
+    }),
+    [isExpanded]
+  );
+
+  return (
+    <div
+      className={`cursor-pointer ${className ? className : ""}`}
+      onClick={toggleIsExpanded}
+    >
+      <div className="flex flex-row w-full items-center">
+        <div className="flex-1">{header}</div>
+        <ArrowDownIcon
+          className={`${
+            isExpanded ? "rotate-180" : "rotate-0"
+          } transition-transform ease-linear`}
+        />
+      </div>
+
+      <div
+        ref={childrenContainerRef}
+        className={`overflow-hidden transition-[height] duration-300`}
+        style={childrenContainerStyle}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
