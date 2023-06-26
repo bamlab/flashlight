3def3b9327cd7f6f28e31d77b8ad597640fbb9b4
Alexandre Moureaux <Almouro@users.noreply.github.com>
2023-06-26 14:23:57 +0200

    chore(measure): upgrade socket io (#129)
    * chore(measure): upgrade socket.io
    
    * chore: ensure yarn.lock gets committed
    
    * chore: update yarn.lock
    
    ---------
    
    Co-authored-by: flashlight-bot <bam.flashlight@gmail.com>

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,3 +1,5 @@
+import { TextEncoder, TextDecoder } from "util";
+
 process.env.AWS_ACCESS_KEY_ID = "MOCK_AWS_ACCESS_KEY_ID";
 process.env.AWS_SECRET_ACCESS_KEY = "MOCK_AWS_SECRET_ACCESS_KEY";
 
@@ -6,3 +8,10 @@ jest.mock("react-apexcharts", () => "apex-charts");
 jest.mock("apexcharts", () => ({ exec: jest.fn() }));
 
 Math.random = jest.fn(() => 0.5);
+
+// Needed to be able to test socket.io v>=4.7.0 in a jsdom environment
+// We use jsdom since we snapshot the webapp in the measure command while running the socket
+global.TextEncoder = TextEncoder;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+global.TextDecoder = TextDecoder;
