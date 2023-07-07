552b70d651b4e491801daebc5f6f6918478cd5d3
Matthieu <62448416+mbezard@users.noreply.github.com>
2023-07-07 11:12:33 +0200

    feat: make the video panel collapsible with animation (#136)
    * feat: make the video panel collapsible with animation
    
    * fix: recomment testCaseResults
    
    * feat: center vertically the videos
    
    ---------
    
    Co-authored-by: Alexandre Moureaux <Almouro@users.noreply.github.com>

--- packages/web-reporter-ui/src/sections/VideoSection.tsx
+++ packages/web-reporter-ui/src/sections/VideoSection.tsx
@@ -1,7 +1,15 @@
-import React, { forwardRef, useCallback, useEffect, useImperativeHandle, useRef } from "react";
+import React, {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
 import { setVideoCurrentTime, useListenToVideoCurrentTime } from "../../videoCurrentTimeContext";
 import { TestCaseResult } from "@perf-profiler/types";
 import { Button } from "../components/Button";
+import { ArrowDownIcon } from "../components/icons/ArrowDownIcon";
 
 const getFileName = (path: string | undefined = ""): string => {
   if (path.startsWith("http")) {
@@ -72,6 +80,8 @@ const Video = forwardRef<
 
 export const VideoSection = ({ results }: { results: TestCaseResult[] }) => {
   const videoRefs = results.map(() => React.createRef<VideoHandle>());
+  const [isPanelExpanded, setIsPanelExpanded] = useState(false);
+  const togglePanel = () => setIsPanelExpanded((prevIsPanelExpanded) => !prevIsPanelExpanded);
 
   const resetVideos = () => {
     // Nice to start a bit before start of measures to see app opening for instance
@@ -91,59 +101,73 @@ export const VideoSection = ({ results }: { results: TestCaseResult[] }) => {
   }, []);
 
   return (
-    <div className="flex flex-col h-full justify-center bg-dark-charcoal p-8 overflow-x-auto flex-shrink-0 max-w-[50vw]">
-      <div className="flex flex-row">
-        {results.length > 1 ? (
-          <div className="flex flex-row">
-            <Button
-              onClick={(event) => {
-                event.stopPropagation();
-                resetVideos();
-              }}
-            >
-              Reset
-            </Button>
-            <div className="w-2" />
-            <Button
-              onClick={(event) => {
-                event.stopPropagation();
-                playVideos();
-              }}
-            >
-              Play all
-            </Button>
-          </div>
-        ) : null}
+    <div
+      className={`flex flex-row flex-nowrap h-full justify-center bg-dark-charcoal overflow-auto py-8 flex-shrink-0 transition-[max-width] ${
+        isPanelExpanded ? "max-w-[24px]" : "max-w-[50vw]"
+      }`}
+    >
+      <div className="self-center py-8 cursor-pointer" onClick={togglePanel}>
+        <ArrowDownIcon
+          size={24}
+          className={`transition-transform ease-linear ${
+            isPanelExpanded ? "rotate-90" : "-rotate-90"
+          }`}
+        />
       </div>
-      <div className="flex flex-1 flex-row max-h-[600px]">
-        {results.map(({ name, iterations: [iteration] }, index) => {
-          const video = iteration.videoInfos;
-
-          return (
-            <div key={index} style={{ width: VIDEO_SIZE.width }} className="flex flex-1 flex-col">
-              {results.length > 1 ? <h6 className="text-white truncate m-1">{name}</h6> : null}
-              {video ? (
-                <Video
-                  video={{
-                    ...video,
-                    startOffset:
-                      /**
-                       * Point for x = 0 in the graph actually corresponds to the first 500ms of measure
-                       * So we need to add 500ms (iteration.measures[0].time) to the startOffset to
-                       * have the video start at the right time
-                       *
-                       * we divide by 2 at the moment to center the measure in the video but we should
-                       * rethink how we display the graph
-                       */
-                      video.startOffset + iteration.measures[0].time / 2,
-                  }}
-                  key={video.path}
-                  ref={videoRefs[index]}
-                />
-              ) : null}
+      <div className="flex flex-col justify-center overflow-hidden">
+        <div className="flex flex-row">
+          {results.length > 1 ? (
+            <div className="flex flex-row">
+              <Button
+                onClick={(event) => {
+                  event.stopPropagation();
+                  resetVideos();
+                }}
+              >
+                Reset
+              </Button>
+              <div className="w-2" />
+              <Button
+                onClick={(event) => {
+                  event.stopPropagation();
+                  playVideos();
+                }}
+              >
+                Play all
+              </Button>
             </div>
-          );
-        })}
+          ) : null}
+        </div>
+        <div className="flex flex-1 flex-row max-h-[600px] gap-4 overflow-scroll pr-6">
+          {results.map(({ name, iterations: [iteration] }, index) => {
+            const video = iteration.videoInfos;
+
+            return (
+              <div key={index} style={{ width: VIDEO_SIZE.width }} className="flex flex-col">
+                {results.length > 1 ? <h6 className="text-white truncate m-1">{name}</h6> : null}
+                {video ? (
+                  <Video
+                    video={{
+                      ...video,
+                      startOffset:
+                        /**
+                         * Point for x = 0 in the graph actually corresponds to the first 500ms of measure
+                         * So we need to add 500ms (iteration.measures[0].time) to the startOffset to
+                         * have the video start at the right time
+                         *
+                         * we divide by 2 at the moment to center the measure in the video but we should
+                         * rethink how we display the graph
+                         */
+                        video.startOffset + iteration.measures[0].time / 2,
+                    }}
+                    key={video.path}
+                    ref={videoRefs[index]}
+                  />
+                ) : null}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
