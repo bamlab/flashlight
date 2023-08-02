85ce1c15400ad1e28a67e9b3417c9ee51aee5c6c
Guillaume Egret <guillaumeeg@bam.tech>
2023-08-02 15:16:08 +0200

    chore(ios_e2e): update script and ghub action job to run ios-test in the CI

--- .github/workflows/ios_e2e.sh
+++ .github/workflows/ios_e2e.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+APPID="org.reactjs.native.example.fakeStore"
+
 # Get the UDID of an iPhone 11 simulator
 UDID=$(xcrun simctl list devices | grep -m 1 'iPhone 14 (' | awk -F '[()]' '{print $2}')
 
@@ -8,3 +10,12 @@ xcrun simctl boot $UDID
 
 # Echo the UDID (for further use)
 echo $UDID
+
+# Install the app
+xcrun simctl install $UDID ./.github/workflows/fakeStore.app
+
+# Launch the app
+xcrun simctl launch $UDID $APPID
+
+mkdir -p report
+npx flashlight-ios-poc ios-test --appId $APPID --simulatorId $UDID --testCommand 'maestro test ./packages/ios-poc/test.yaml' --resultsFilePath './report/result.json'
