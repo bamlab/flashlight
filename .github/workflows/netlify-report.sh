7dac9abd56fa4ce8489d43fd44f4b327b39666a0
Guillaume Egret <guillaumeeg@bam.tech>
2023-08-21 11:33:06 +0200

    chore(netlify-report): fix grep regex

--- .github/workflows/netlify-report.sh
+++ .github/workflows/netlify-report.sh
@@ -3,7 +3,7 @@
 set -e
 
 OUTPUT=$(npx netlify-cli@15.11.0 deploy --site $NETLIFY_SITE_ID --dir report --auth $NETLIFY_DEPLOY_TOKEN)
-NETLIFY_DEPLOY_PREVIEW_URL=$(echo $OUTPUT | grep "Website Draft URL:" | sed -E 's/(.*)Website Draft URL: //g' | sed -E 's/ If everything looks(.*)//g')/report.html
+NETLIFY_DEPLOY_PREVIEW_URL=$(echo $OUTPUT | grep "Website [Dd]raft URL:" | sed -E 's/(.*)Website [Dd]raft URL: //g' | sed -E 's/ If everything looks(.*)//g')/report.html
 echo "✅ Site deployed to: $NETLIFY_DEPLOY_PREVIEW_URL"
 
 echo "NETLIFY_PREVIEW_URL=$NETLIFY_DEPLOY_PREVIEW_URL" >> $GITHUB_ENV
