7f77914b09e604270304965157aa14612c3bf331
Guillaume Egret <guillaumeeg@bam.tech>
2023-08-21 15:11:21 +0200

    chore(netlify-report): add a check to be able to run in ghub action and bitrise

--- .github/workflows/netlify-report.sh
+++ .github/workflows/netlify-report.sh
@@ -6,4 +6,11 @@ OUTPUT=$(npx netlify-cli@15.11.0 deploy --site $NETLIFY_SITE_ID --dir report --a
 NETLIFY_DEPLOY_PREVIEW_URL=$(echo $OUTPUT | grep "Website [Dd]raft URL:" | sed -E 's/(.*)Website [Dd]raft URL: //g' | sed -E 's/ If everything looks(.*)//g')/report.html
 echo "✅ Site deployed to: $NETLIFY_DEPLOY_PREVIEW_URL"
 
-echo "NETLIFY_PREVIEW_URL=$NETLIFY_DEPLOY_PREVIEW_URL" >> $GITHUB_ENV
+# check if $GITHUB_ENV is set
+if [ -z "$GITHUB_ENV" ]; then
+  envman add --key NETLIFY_DEPLOY_PREVIEW_URL --value "$NETLIFY_DEPLOY_PREVIEW_URL"
+else
+    echo "NETLIFY_PREVIEW_URL=$NETLIFY_DEPLOY_PREVIEW_URL" >> $GITHUB_ENV
+fi
+
+
