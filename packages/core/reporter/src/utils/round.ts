0c3e2a47bdccce6a1573d1ec9f234b564015e523
charles BOULENGE <charlesbo@bam.tech>
2023-12-05 16:47:04 +0100

    snapshot: changes since we use lodash round function now

--- packages/core/reporter/src/utils/round.ts
+++ packages/core/reporter/src/utils/round.ts
@@ -1,6 +1,3 @@
-export const round = (n: number, decimals: number) =>
-  Math.floor(n * Math.pow(10, decimals)) / Math.pow(10, decimals);
-
 export const roundToDecimal = (value: number, decimalCount = 1) => {
   const factor = Math.pow(10, decimalCount);
   return Math.round(value * factor) / factor;
