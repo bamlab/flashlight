254aa1247e3c0a1570895935003685c994393a02
almouro <contact@almouro.com>
2023-12-22 17:45:47 +0100

    refactor(aws): simplify ffmpeg install logic

--- packages/core/shell/src/processVideoFile.ts
+++ packages/core/shell/src/processVideoFile.ts
@@ -21,34 +21,30 @@ const execAsync = (command: string) =>
     proc.on("error", reject);
   });
 
+export const installFFMpeg = async () => {
+  await execAsync(`mkdir -p ${FFMPEG_BINARY_FOLDER_PATH}`);
+
+  // Download ffmpeg binary
+  await downloadFile(
+    // "https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v4.4.1/ffmpeg-4.4.1-linux-32.zip",
+    "https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v4.4.1/ffmpeg-4.4.1-osx-64.zip",
+    FFMPEG_BINARY_FOLDER_PATH + "/ffmpeg.zip"
+  );
+
+  unzip(FFMPEG_BINARY_FOLDER_PATH + "/ffmpeg.zip", FFMPEG_BINARY_FOLDER_PATH);
+
+  // Make the binary executable
+  await execAsync(`chmod +rwx ${FFMPEG_BINARY_FOLDER_PATH}/ffmpeg`);
+};
+
 export const processVideoFile = async (filePath: string, destinationPath: string) => {
+  const ffmpegExecutable = fs.existsSync(`${FFMPEG_BINARY_FOLDER_PATH}/ffmpeg`)
+    ? `${FFMPEG_BINARY_FOLDER_PATH}/ffmpeg`
+    : "ffmpeg";
+
   // When coming from AWS Device Farm or certain devices, it seems the video is not encoded properly
   // VSync 0 is important since we have variable frame rate from adb shell screenrecord
   await execAsync(
-    `ffmpeg -y -vsync 0 -i ${filePath} -c:v libx264 -crf 23 -c:a aac -b:a 128k ${destinationPath} -loglevel error`
-  ).catch(async (err) => {
-    // If the error is ENOENT, then it suggests that ffmpeg is not installed. It could be the case
-    // since the ffmpeg package is not available on AWS machines with Node 18.
-    if (err.code === "ENOENT") {
-      if (!fs.existsSync(`${FFMPEG_BINARY_FOLDER_PATH}/ffmpeg`)) {
-        await execAsync(`mkdir -p ${FFMPEG_BINARY_FOLDER_PATH}`);
-
-        // Download ffmpeg binary
-        await downloadFile(
-          // "https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v4.4.1/ffmpeg-4.4.1-linux-32.zip",
-          "https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v4.4.1/ffmpeg-4.4.1-osx-64.zip",
-          FFMPEG_BINARY_FOLDER_PATH + "/ffmpeg.zip"
-        );
-
-        unzip(FFMPEG_BINARY_FOLDER_PATH + "/ffmpeg.zip", FFMPEG_BINARY_FOLDER_PATH);
-
-        // Make the binary executable
-        await execAsync(`chmod +rwx ${FFMPEG_BINARY_FOLDER_PATH}/ffmpeg`);
-      }
-
-      await execAsync(
-        `${FFMPEG_BINARY_FOLDER_PATH}/ffmpeg -y -vsync 0 -i ${filePath} -c:v libx264 -crf 23 -c:a aac -b:a 128k ${destinationPath} -loglevel error`
-      );
-    }
-  });
+    `${ffmpegExecutable} -y -vsync 0 -i ${filePath} -c:v libx264 -crf 23 -c:a aac -b:a 128k ${destinationPath} -loglevel error`
+  );
 };
