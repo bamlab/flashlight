241d5b275944e7fc1aa7f963304a69c7d4d7c2a9
almouro <contact@almouro.com>
2024-01-17 15:59:16 +0100

    chore: prevent jest for failing for ts errors
    Seemingly Jest can fail even though tsc is passing properly
    Since we catch errors in "yarn tsc" already, we don't need jest to bother us about it anyway
    I think ts-jest doesn't recognize properly the .d.ts definition basically

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 const commonOptions = {
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        // we already get TS errors with tsc, so no need to have them here
+        diagnostics: false,
+      },
+    ],
   },
   testPathIgnorePatterns: [
     "\\.snap$",
