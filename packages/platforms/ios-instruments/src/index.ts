5690f047af42e72d5371454890c0978d91cfe666
Guillaume Egret <guillaumeeg@bam.tech>
2024-01-18 17:33:53 +0100

    feat(instruments profiler): add new profiler

--- packages/platforms/ios-instruments/src/index.ts
+++ packages/platforms/ios-instruments/src/index.ts
@@ -1,1 +1,40 @@
+import { Measure, Profiler, ProfilerPollingOptions, ScreenRecorder } from "@perf-profiler/types";
 export { killApp } from "./utils/DeviceManager";
+
+export class IOSInstrumentsProfiler implements Profiler {
+  connectedDevice: IdbDevice | undefined;
+  recordingProcess: ChildProcess | undefined;
+  traceFile: string | undefined;
+  pid: number | undefined;
+  bundleId: string | undefined;
+  onMeasure: ((measure: Measure) => void) | undefined;
+  pollPerformanceMeasures(bundleId: string, options: ProfilerPollingOptions): { stop: () => void } {
+    if (!this.pid) throw new Error("Profiler is not ready, app is not running");
+    this.onMeasure = options.onMeasure;
+    return {
+      stop: () => {
+        return;
+      },
+    };
+  }
+
+  detectCurrentBundleId(): string {
+    throw new Error("App Id detection is not implemented on iOS with Instruments");
+  }
+
+  installProfilerOnDevice() {
+    // Do we need anything here?
+  }
+
+  getScreenRecorder(videoPath: string): ScreenRecorder | undefined {
+    return undefined;
+  }
+
+  cleanup: () => void = () => {
+    // Do we need anything here?
+  };
+  async stopApp(bundleId: string): Promise<void> {
+    killApp(bundleId);
+    return new Promise<void>((resolve) => resolve());
+  }
+}
