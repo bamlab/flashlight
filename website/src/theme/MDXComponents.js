73a0dd20811f95af9942d67f56f4961093569b04
hduprat <hugod@bam.tech>
2024-01-18 15:52:09 +0100

    chore(MDXComponents): extract module contents to variable

--- website/src/theme/MDXComponents.js
+++ website/src/theme/MDXComponents.js
@@ -4,10 +4,12 @@ import React from "react";
 import MDXComponents from "@theme-original/MDXComponents";
 import Contact from "@site/src/components/Contact.mdx";
 
-export default {
+const _module = {
   // Re-use the default mapping
   ...MDXComponents,
   // Map the "<Highlight>" tag to our Highlight component
   // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
   Contact,
 };
+
+export default _module;
