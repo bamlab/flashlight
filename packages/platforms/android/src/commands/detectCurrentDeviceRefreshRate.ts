64b535b03e3eb31b68519963cd4b51be01cc53fd
Tomi Alu <tomi@thoughtbot.com>
2024-09-06 13:24:40 +0100

    fix(profiler): accomodate varying refresh rate configurations
    Some devices come with the capabilities of switching between 60,90, 120 fps. The renderFrameRate reports the currently configured refresh rate.

--- packages/platforms/android/src/commands/detectCurrentDeviceRefreshRate.ts
+++ packages/platforms/android/src/commands/detectCurrentDeviceRefreshRate.ts
@@ -20,6 +20,13 @@ export const detectCurrentDeviceRefreshRate = () => {
   const command = 'adb shell dumpsys display | grep -E "mRefreshRate|DisplayDeviceInfo"';
   const commandOutput = executeCommand(command);
 
+  const renderFrameRateMatch = commandOutput.match(/renderFrameRate\s+(\d+\.?\d*)/);
+
+  if (renderFrameRateMatch) {
+    Logger.debug(`Detected device refresh rate: ${renderFrameRateMatch[1]} Hz`);
+    return Math.floor(parseFloat(renderFrameRateMatch[1]));
+  }
+
   const matches = commandOutput.matchAll(/fps=(\d+\.?\d*)/g);
   const refreshRates = Array.from(matches, (match) => parseFloat(match[1]));
   refreshRates.sort((a, b) => b - a);
