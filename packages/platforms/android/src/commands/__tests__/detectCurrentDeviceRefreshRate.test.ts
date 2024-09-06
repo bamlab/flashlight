c1d658ad0d93dcab01377985bcf4d90a54bd1af8
Tomi Alu <tomi@thoughtbot.com>
2024-09-06 13:41:05 +0100

    test: add new specs for testing refresh rates on Pixels

--- packages/platforms/android/src/commands/__tests__/detectCurrentDeviceRefreshRate.test.ts
+++ packages/platforms/android/src/commands/__tests__/detectCurrentDeviceRefreshRate.test.ts
@@ -2,11 +2,12 @@ import { detectCurrentDeviceRefreshRate } from "../detectCurrentDeviceRefreshRat
 import fs from "fs";
 
 const sampleOutput = fs.readFileSync(`${__dirname}/dumpsys-display.txt`, "utf-8");
+const sampleOutput2 = fs.readFileSync(`${__dirname}/dumpsys-display120.txt`, "utf-8");
 
 const executeCommandSpy = jest.spyOn(require("../shell"), "executeCommand");
 
 describe("detectCurrentDeviceRefreshRate", () => {
-  it("retrieves correctly device refresh rate", () => {
+  it("retrieves correctly device refresh rate of a basic 60fps device", () => {
     executeCommandSpy.mockImplementation((command) => {
       expect(command).toEqual(
         'adb shell dumpsys display | grep -E "mRefreshRate|DisplayDeviceInfo"'
@@ -16,6 +17,18 @@ describe("detectCurrentDeviceRefreshRate", () => {
     });
 
     expect(detectCurrentDeviceRefreshRate()).toEqual(60);
+  });
+
+  it("retrieves correctly device refresh rate of a 120fps pixel device", () => {
+    executeCommandSpy.mockImplementation((command) => {
+      expect(command).toEqual(
+        'adb shell dumpsys display | grep -E "mRefreshRate|DisplayDeviceInfo"'
+      );
+
+      return sampleOutput2;
+    });
+
+    expect(detectCurrentDeviceRefreshRate()).toEqual(120);
   });
 
   it("throws an error in case it couldn't find it", () => {
